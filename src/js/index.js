cacb31327ba28c5302f62dfb7ea6ecab22800f68
Nicolas <nicolaskrusser@gmail.com>
2019-11-10 16:33:08 +0100

    v1

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+
+// FOUNDATION
+// ========================================
+import {
+  Core as Foundation,
+  // Abide,
+  // Accordion,
+  AccordionMenu,
+  // Drilldown,
+  // Dropdown,
+  DropdownMenu,
+  // Equalizer,
+  // Interchange,
+  // Magellan,
+  OffCanvas,
+  // Orbit,
+  // ResponsiveMenu,
+  // ResponsiveToggle,
+  // Reveal,
+  // Slider,
+  // SmoothScroll,
+  // Sticky,
+  // Tabs,
+  // Toggler,
+  // Tooltip,
+  // ResponsiveAccordionTabs,
+} from 'foundation-sites';
+
+Foundation.addToJquery($);
+
+
+
+$(function() {
+  // FOUNDATION
+  $(document).foundation();
+
+});
